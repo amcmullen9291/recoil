3e2c0e91f4eb333de60aa5842ee3aecdb8c841d4
amcmullen <amcmullen9291@gmail.com>
2022-01-17 11:39:30 -0500

    app structure complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Header } from './Components/Header'
+import { Balance } from './Components/Balance'
+import { ExpenseStatus } from './Components/ExpenseStatus'
+import { TransactionList } from './Components/TransactionList'
+import { NewTransaction } from './Components/NewTransaction'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <Balance />
+                <ExpenseStatus />
+                <TransactionList />
+                <NewTransaction />
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { RecoilRoot } from "recoil"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
+// nothing at all changes in this file. Put the RecoilRoot  component where the Store would be
